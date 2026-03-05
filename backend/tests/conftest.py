d0fdbc97acf13bf14afd22fa853a9b788ffadd9d
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2026-03-05 11:24:42 +0530

    test_app.py is removed
    Because it mixes:root test,database fixture,dependency override

---
+++ backend/tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.db.database import Base
+from app.routes.auth import get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+TestingSessionLocal = sessionmaker(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture(scope="function")
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def client():
+    return TestClient(app)
---
+++ backend/tests/test_login.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session
+from app.db.models import User
+from app.core.security import hash_password
+from app.routes.auth import get_db
+
+
+def test_login_success(client, test_db):
+
+    # get test database session
+    db: Session = next(get_db())
+
+    # create fake user
+    user = User(
+        username="amantha@gmail.com",
+        password=hash_password("amantha")
+    )
+
+    db.add(user)
+    db.commit()
+
+    # correct login request
+    response = client.post("/login", json={
+        "username": "amantha@gmail.com",
+        "password": "amantha"
+    })
+
+    assert response.status_code == 200
